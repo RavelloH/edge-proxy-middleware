7db9df117b21c88bf53b36d9d26ea3aabcc8c1f6
RavelloH <hyh20060327@qq.com>
2023-11-05 15:11:25 +0800

    Feat.切换至edge function

--- api/edge.js
+++ api/edge.js
@@ -1,25 +1,25 @@
 export const config = {
-    runtime: 'edge',
+  runtime: 'edge',
 };
 
 export default async function handler(req, res) {
-    const { url } = req.query;
- 
-    if (!url) {
-        res.statusCode = 400;
-        res.end('Missing url parameter');
-        return;
-    }
+  const { url } = req.query;
 
-    try {
-        const response = await fetch(url);
-        const contentType = response.headers.get('content-type');
-        res.setHeader('Content-Type', contentType);
+  if (!url) {
+    res.statusCode = 400;
+    res.end('Missing url parameter');
+    return;
+  }
 
-        response.body.pipe(res);
-    } catch (err) {
-        console.error(`Error: ${err.message}`);
-        res.statusCode = 500;
-        res.end('Internal Server Error');
-    }
-};
+  try {
+    const response = await fetch(url);
+    const contentType = response.headers.get('content-type');
+    res.setHeader('Content-Type', contentType);
+
+    response.body.pipe(res);
+  } catch (err) {
+    console.error(`Error: ${err.message}`);
+    res.statusCode = 500;
+    res.end('Internal Server Error');
+  }
+}
